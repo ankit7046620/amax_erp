13950c7a6788167d6927304e3d0facf76b059ddd
vpprajapati19 <31657261+vpprajapati19@users.noreply.github.com>
2025-08-07 18:24:26 +0530

    Employees Checkin popup page done

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -46,9 +46,9 @@ class LoginController extends GetxController {
   setData() {
     emailController.text = "vignesh@amaxconsultancyservices.com";
     passwordController.text = "Welcome@@123#";
-    //
-    // emailController.text = "ankit22@yopmail.com";
-    // passwordController.text = "Test@123";
+
+   //  emailController.text = "ankit22@yopmail.com";
+   //  passwordController.text = "Test@123";
   }
 
   void togglePasswordVisibility() {
